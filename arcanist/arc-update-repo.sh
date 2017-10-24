298b018a6ff30872d131ae21950aa8c690332a57
Sébastien Santoro <dereckson@espace-win.org>
2017-10-24 12:18:53 +0000

    Improve Arcanist shell scripts
    Summary:
    Fixes suggested by shellcheck:
      - use $() instead of deprecated ``
      - avoid cat when pipelines can be used
      - quote variables
    
    Test Plan: `shellcheck`
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D1118

--- arcanist/arc-update-repo.sh
+++ arcanist/arc-update-repo.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-ROOT_DIR=`git rev-parse --show-toplevel 2>/dev/null`
+ROOT_DIR=$(git rev-parse --show-toplevel 2>/dev/null)
 if [ "$?" -ne "0" ]; then
-	ROOT_DIR=`hg root 2>/dev/null`
+	ROOT_DIR=$(hg root 2>/dev/null)
 	if [ "$?" -ne "0" ]; then
 		echo "Doesn't seem to be a Git or Mercurial repository."
 		exit
 	fi
 fi
 
-if [ ! -f $ROOT_DIR/.arcconfig ]; then
+if [ ! -f "$ROOT_DIR/.arcconfig" ]; then
 	echo 'Create a .arcconfig file with "repository.callsign" : "..." (without the leading r) option.'
 	exit
 fi
 
-CALLSIGN=`cat $ROOT_DIR/.arcconfig | jq '."repository.callsign"'`
+CALLSIGN=$(jq '."repository.callsign"' < "$ROOT_DIR/.arcconfig")
 if [ "$CALLSIGN" = "null" ]; then
 	echo 'Add to your .arcconfig file a "repository.callsign" : "..." (without the leading r) option.'
 	exit
